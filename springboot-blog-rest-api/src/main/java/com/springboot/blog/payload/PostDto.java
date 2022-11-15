8cc92bd56f88e27e113fa61d737a440f5954b7cd
Yuktageo <yuktajanuary@gmail.com>
2022-11-15 18:00:19 +0900

    Validate create post and update post REST API

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/PostDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,14 +2,21 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 //RequestBody for converting the JSON into the java object by using http message converter
 @Data
 public class PostDto {
     private long id;
+    @NotEmpty
+    @Size(min = 2,message = "Post title should have at least two characters")
     private String title;
+    @NotEmpty
+    @Size(min = 10,message = "Description should have atleast ten characters")
     private String description;
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 
