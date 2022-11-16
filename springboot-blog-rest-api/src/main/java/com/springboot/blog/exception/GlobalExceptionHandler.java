c507c7f4c365215b5bb95ace3151837b1be72ef9
Yuktageo <yuktajanuary@gmail.com>
2022-11-16 13:50:12 +0900

    Global exception handler

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -1,20 +1,26 @@
 package com.springboot.blog.exception;
 
 import com.springboot.blog.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 //Controller advice is used to handle the exception globally.@ControllerAdvice ANNOTATION IS HAVING @Component annotation too which itself creates the bean
 //ApplicationContext is where spring holds the instances of all the objects that is has identified to be managed and distribute it automatically
 //Using the IOC method spring collects all the beans from our application and return it whenever it is required in the application at the run time
 //@Component annotation allow spring to automatically detect our custom beans .Spring scan our classes annotated with @Component
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     //handle specific exception and send it back to the client
     @ExceptionHandler(BlogApiException.class)
@@ -34,4 +40,21 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails=new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
+    //Customize the validation response and for that you need to extend ResponseEntityExceptionHandler  and you need to override handlemethodArgumentNotValid
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String,String>  errors=new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String message=error.getDefaultMessage();
+
+            errors.put(fieldName,message);
+        });
+
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+
 }
